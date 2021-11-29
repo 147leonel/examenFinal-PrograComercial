622b89de3ebcea71d592ac26a0045dd15967e29f
147leonel <ser.leo.147@gmail.com>
2021-11-29 16:37:55 -0600

    added urls

---
+++ colegio/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    #url('', views.lista_peliculas, name ='lista_peliculas'),
+    url('curso/nuevo/', views.curso_nuevo, name='curso_nuevo'),
+    ]
