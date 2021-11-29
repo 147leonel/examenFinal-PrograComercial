3c0cdbc4e7a5ada9e1d8ca639043bc5391d0cd5f
147leonel <ser.leo.147@gmail.com>
2021-11-29 16:19:04 -0600

    created models and migrations

--- colegio/models.py
+++ colegio/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class Alumno(models.Model):
+    carne= models.CharField(max_length=9)
+    nombre  =   models.CharField(max_length=50)
+    apellidos =models.CharField(max_length=75)
+    fecha_nacimiento = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+
+class Curso(models.Model):
+    nombre    = models.CharField(max_length=60)
+    alumnos   = models.ManyToManyField(Alumno, through='Cursando')
+
+    def __str__(self):
+        return self.nombre
+
+class Cursando (models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+class CursandoInLine(admin.TabularInline):
+    model = Cursando
+#muestra una linea extra al momento de insertar, como indicación al usuario que se pueden ingresar varios actores.
+    extra = 1
+
+
+class AlumnoAdmin(admin.ModelAdmin):
+    inlines = (CursandoInLine,)
+
+class CursoAdmin (admin.ModelAdmin):
+    inlines = (CursandoInLine,)
